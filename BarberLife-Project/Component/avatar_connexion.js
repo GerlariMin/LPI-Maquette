41cfbe0ced62e7539279ef8cd40731346ea4d78d
Hassane BARO <Hassane BARO>
2020-06-08 23:36:18 +0200

    ConnexionFrontAndBack

---
+++ BarberLife-Project/Component/avatar_connexion.js
@@ -0,0 +1,29 @@
+//Components/cAvatar.js
+import React from 'react';
+/*
+  react-native-elements:
+  https://react-native-elements.github.io/react-native-elements/docs/getting_started.html
+*/
+import { Avatar } from 'react-native-elements';
+
+/*
+  AVATAR:
+  https://react-native-elements.github.io/react-native-elements/docs/avatar.html
+*/
+class cHeader extends React.Component
+{
+  render()
+  {
+    // Standard Avatar with accessory
+    return(
+      <Avatar
+        rounded
+        title="MM"
+        overlayContainerStyle={{backgroundColor: 'brown'}}
+        showAccessory
+      />
+    )
+  }
+}
+
+export default cHeader
--- BarberLife-Project/Component/page_connexion.js
+++ BarberLife-Project/Component/page_connexion.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import {View,StyleSheet,Text,TextInput,Button,TouchableOpacity,SafeAreaView} from 'react-native'
+import {View,StyleSheet,Text,TextInput,TouchableOpacity,SafeAreaView} from 'react-native'
 // Sert pour mettre la ligne de délimitation
-import Divider from 'react-native-divider'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Card, Input,Button } from 'react-native-elements';
 
 
 
@@ -63,115 +64,106 @@ class Connexion extends React.Component{
             }),
         })
         const users = await response.json();
-        //console.log(users);
+        
     }
     getConnexion(){
-       
         this.fetchConnexion(); 
     }
 
     render(){
         return(
-            //Permet de garder le haut de l'écran avec la batterie et le réseaux
-            <SafeAreaView style={styles.container}>
-                <View style={styles.content}>
-                    <Text style={styles.title}>
-                        <Text style={styles.title_blue}>Bar</Text>
-                        <Text>Ber</Text>
-                        <Text style={styles.title_red}>LiFe</Text>
-                    </Text>
-                    <TextInput style={styles.idInput} placeholder="Numéro de téléphone,nom d'utilisateur ou a.." onChangeText={(text) => this.getInputId(text)}/>
-                    <TextInput style={styles.mdpInput} placeholder="Mot de passe" textContentType={'password'} secureTextEntry={true}  onChangeText={(text) => this.getInputMdp(text)}/>
-                    <TouchableOpacity style={styles.valideInput} >
-                        <Button title="Connexion" onPress={() => this.getConnexion()}/>
-                    </TouchableOpacity>
-                    <Divider orientation="center">OU</Divider>
-                    <TouchableOpacity style={styles.headerInscrip}>
-                        <Text style={styles.inscripText}>
-                            <Text>Vous n'avez pas de compte ? </Text>    
-                            <Text style={styles.inscripButton}>
-                                Inscrivez-vous 
-                            </Text>
-                        </Text>   
-                    </TouchableOpacity>
-                    <TouchableOpacity>
-                        <Text style={styles.forgetMdp}>Mot de passe oublié ?</Text>
-                    </TouchableOpacity>
-                    
-                </View>
-            </SafeAreaView>
+            <Card
+            title='Connexion'
+            image={require('../Images/BarberLife-logo-Orange.png')}
+            containerStyle={{ borderRadius: '25px', opacity: 0.98, height: '95%' }}
+          >
+            <View>
+            <Input
+              label='Login/ Mail'
+              labelStyle={{ fontWeight: 'bold', fontStyle: 'italic' }}
+              placeholder='Saisissez votre login/ mail'
+              leftIcon=
+              {
+                <Icon
+                  name='user-circle-o'
+                  size={24}
+                  color='black'
+                />
+              }
+              errorMessage='ENTER A VALID LOGIN'
+              errorStyle={{ color: 'red', fontWeight: 'bold' }}
+              onChangeText={(text) => this.getInputId(text)}
+            />
+    
+            <Input
+              label='Mot de passe'
+              labelStyle={{ fontWeight: 'bold', fontStyle: 'italic' }}
+              placeholder="Saisissez votre mot de passe"
+              secureTextEntry={true}
+              leftIcon=
+              {
+                <Icon
+                  name='unlock-alt'
+                  size={24}
+                  color='black'
+                />
+              }
+              errorMessage='ENTER A VALID PASSWORD'
+              errorStyle={{ color: 'red', fontWeight: 'bold' }}
+              onChangeText={(text) => this.getInputMdp(text)}
+            />
+            </View>
+    
+            <View style={styles.space}>
+            <Text
+              style={styles.center}
+              > Vous ne possédez pas encore de compte ? </Text>
+            <Text
+              accessibilityRole='link'
+              style={[styles.center, styles.link]}
+              > Inscrivez-vous </Text>
+            </View>
+    
+            <View>
+            <Button
+              buttonStyle={{bottom: 0}}
+              icon=
+              {
+                <Icon
+                  name="beer"
+                  size={15}
+                  color="white"
+                />
+              }
+              loading={this.state.loading}
+              onPress={() => this.getConnexion()}
+              title=" Connexion"
+              type='solid'
+            />
+            </View>
+    
+          </Card>
         )
     }
 }
 
-const styles = StyleSheet.create({
-    container:{
-        //backgroundColor:'red',
-        alignItems:"center",
-        justifyContent:"center",
-        flex:1   
-    },
-    content:{
-       //backgroundColor:'blue',
-        width:350,
-        height:350
-    },
-    title:{
-        fontSize:40,
-        textAlign:"center",
-    
-    },
-    idInput:{
-        height:40,
-        width:250,
-        borderRadius:5,
-        borderColor:"black",
-        borderWidth:2,
-        marginTop:20,
-        marginLeft:50,
-        marginRight:50
-    },
-    mdpInput:{
-        height:40,
-        width:250,
-        borderRadius:5,
-        borderColor:"black",
-        borderWidth:2,
-        marginTop:20,
-        marginRight:50,
-        marginLeft:50
-    }
-    ,
-    title_blue:{
-        color:"blue"
-    },
-    title_red:{
-        color:"red"
-    },
-    valideInput:{
-        marginTop:10, 
-    },
-    headerInscrip:{
-        flexDirection:"row",
-        marginTop:15,
-        alignItems:"center",
-        justifyContent:"center"
-    },
-    inscripText:{
-        
-        flexWrap:'wrap',
-        paddingRight:10
-    },
-    inscripButton:{
-        fontSize:18,
-        color:"blue",  
-    },
-    forgetMdp:{
-        textAlign:"center",
-        marginTop:10,
-        fontSize:15,
-        color:'grey'
-    }  
-})
+
+const styles = StyleSheet.create(
+    {
+      center:
+      {
+        textAlign: 'center'
+      },
+      link:
+      {
+        color: 'blue',
+        fontWeight: 'bold'
+      },
+      space:
+      {
+        marginTop: 10,
+        marginBottom: 10
+      }
+    });
 
 export default Connexion
